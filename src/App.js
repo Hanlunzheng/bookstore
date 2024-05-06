63bedb799693e93eadbcd14ccb486070268a6136
Hanlun Zheng <hanlunzheng@macbook-pro-3.mynetworksettings.com>
2024-05-06 05:26:27 -0400

    last try

--- src/App.js
+++ src/App.js
@@ -1,67 +1,78 @@
-import Nav from './components/Nav';
+import Nav from "./components/Nav";
 import React, { useEffect, useState } from "react";
-import Footer from './components/Footer';
-import { BrowserRouter as Router, Route} from 'react-router-dom'
-import Home from './pages/Home';
-import Books from './pages/Books';
-import { books } from './data'
-import BookInfo from './pages/BookInfo';
-import Cart from './pages/Cart';
-
-
-
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
 
 function App() {
   const [cart, setCart] = useState([]);
 
   function addToCart(book) {
-    setCart([...cart, {...book, quantity: 1}])
+    setCart([...cart, { ...book, quantity: 1 }]);
   }
-  function changeQuantity (book, quantity){
-    setCart(cart.map(item => {
-      if (item.id === book.id) {
-        return {
-          ...item,
-          quantity: +quantity,
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) => {
+        if (item.id === book.id) {
+          return {
+            ...item,
+            quantity: +quantity,
+          };
+        } else {
+          return item;
         }
-      }
-        else {
-          return item
-        }
-    }))
+      })
+    );
   }
 
-  function removeItem (item) {
-    setCart(cart.filter(book => book.id !== item.id ))
+  function removeItem(item) {
+    setCart(cart.filter((book) => book.id !== item.id));
   }
 
   function numberOfItems() {
-    let counter = 0
-    cart.forEach(item => {
-      counter += item.quantity
-    })
-    return counter
+    let counter = 0;
+    cart.forEach((item) => {
+      counter += item.quantity;
+    });
+    return counter;
   }
+
+  useEffect(() => {
+    // Set the title based on the current route
+    document.title = "AaronFang design";
+  }, []);
 
   return (
     <Router>
-    <div className="App">
-     
-      <Nav numberOfItems = {numberOfItems()}/>
-      <Route path="/" exact component={Home}/>
-      <Route path="/books" exact render= {() => <Books books={books}/>}/>
-      <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} cart = {cart}/>}/>
-      <Route path="/cart" render= {() => 
-      <Cart books={books} cart = {cart} 
-      changeQuantity = {changeQuantity} 
-      removeItem = {removeItem}/>}/>
-      <Footer/>
-    </div>
+      <div className="App">
+        <Nav numberOfItems={numberOfItems()} />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route
+          path="/books/:id"
+          render={() => (
+            <BookInfo books={books} addToCart={addToCart} cart={cart} />
+          )}
+        />
+        <Route
+          path="/cart"
+          render={() => (
+            <Cart
+              books={books}
+              cart={cart}
+              changeQuantity={changeQuantity}
+              removeItem={removeItem}
+            />
+          )}
+        />
+        <Footer />
+      </div>
     </Router>
   );
 }
 
 export default App;
-
-
-
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import UndrawBooks from '../assets/Undraw_Books.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Bookstore from "../assets/bookstore.png";
 
 const Landing = () => {
-    return (
-        <section id="landing">      
-            <header>
-                <div className="header__container">         
-                    <div className="header__description">
-                        <h1>America's most awarded online library platform</h1>
-                        <h2>Find your dream book with <span className='purple'>Library</span></h2>
-                        <Link to="/books">    
-                            <button className="btn">Browse books</button>
-                        </Link>
-                    </div>
-                    <figure className = 'header__img--wrapper'> 
-                        <img src={UndrawBooks} alt="" />
-                    </figure>
-                </div>
-
-            </header>
-        </section>
-    );
-}
+  return (
+    <section id="landing">
+      <header>
+        <div className="header__container">
+          <div className="header__description">
+            <h1>Weclome to Aaron Fang BookStore</h1>
+            <h2>
+              Find your dream book in{" "}
+              <span className="purple">Aaron BookStore</span>
+            </h2>
+            <Link to="/books">
+              <button className="btn">Browse books</button>
+            </Link>
+          </div>
+          <figure className="header__img--wrapper">
+            <img
+              src={Bookstore}
+              alt=""
+              style={{ width: "300px", height: "auto" }}
+            />
+          </figure>
+        </div>
+      </header>
+    </section>
+  );
+};
 
 export default Landing;
-
